a0d0be210e9bc1c41bfdb599348a6460f310fea8
anushka255 <66816733+anushka255@users.noreply.github.com>
2024-08-06 11:48:44 -0400

    minor update (#1)
     added pytest as dependency; added init file under test directory;  getting rid of glob from test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,23 @@
 from pathlib import Path
-
 import numpy as np
 import scanpy as sc
-import glob
 import pytest
 
 base_dir = Path(__file__).parent.parent
-data_folder = base_dir / "sample_data"
+data_dir = base_dir / "sample_data"
 
 
 @pytest.fixture(scope="session")
 def slices():
-    # Returns ann data object relating each slices
-    slice_files = glob.glob(f"{data_folder}/slice[0-9].csv")
-    coord_files = glob.glob(f"{data_folder}/slice[0-9]_coor.csv")
-
     slices = []
-    for slice, coord in zip(slice_files, coord_files):
-        _slice = sc.read_csv(Path(slice))
-        _slice.obsm["spatial"] = np.genfromtxt(coord, delimiter=",")
+    for i in range(1, 5):
+        # File path of slices and respective coordinates
+        s_fpath = Path(f"{data_dir}/slice{i}.csv")
+        c_fpath = Path(f"{data_dir}/slice{i}_coor.csv")
+
+        # Create ann data object of each slice and add other properties
+        _slice = sc.read_csv(s_fpath)
+        _slice.obsm["spatial"] = np.genfromtxt(c_fpath, delimiter=",")
         _slice.obsm["weights"] = np.ones((_slice.shape[0],)) / _slice.shape[0]
         slices.append(_slice)
 
