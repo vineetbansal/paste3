727c63a5d23714e15814d67feeec2898be6283c8
anushka255 <66816733+anushka255@users.noreply.github.com>
2024-08-27 14:14:32 -0400

    integrating same function s from two helper files (#22)

--- src/paste2/helper.py
+++ src/paste2/helper.py
@@ -7,21 +7,6 @@ import scanpy as sc
 from scipy.spatial import distance
 
 
-
-
-def filter_for_common_genes(slices):
-    """
-    param: slices - list of slices (AnnData objects)
-    """
-    assert len(slices) > 0, "Cannot have empty list."
-    
-    common_genes = slices[0].var.index
-    for s in slices:
-        common_genes = intersect(common_genes, s.var.index)
-    for i in range(len(slices)):
-        slices[i] = slices[i][:, common_genes]
-    print('Filtered all slices for common genes. There are ' + str(len(common_genes)) + ' common genes.')
-
 def kl_divergence(X, Y):
     """
     Returns pairwise KL divergence (over all pairs of samples) of two matrices X and Y.
@@ -137,27 +122,6 @@ def norm_and_center_coordinates(X):
     return: 
     """
     return (X-X.mean(axis=0))/min(scipy.spatial.distance.pdist(X))
-
-
-def match_spots_using_spatial_heuristic(X,Y,use_ot=True):
-    """
-    param: X - numpy array
-    param: Y - numpy array
-    
-    return: pi- mapping of spots using spatial heuristic
-    """
-    n1,n2=len(X),len(Y)
-    X,Y = norm_and_center_coordinates(X),norm_and_center_coordinates(Y)
-    dist = scipy.spatial.distance_matrix(X,Y)
-    if use_ot:
-        pi = ot.emd(np.ones(n1)/n1, np.ones(n2)/n2, dist)
-    else:
-        row_ind, col_ind = scipy.sparse.csgraph.min_weight_full_bipartite_matching(scipy.sparse.csr_matrix(dist))
-        pi = np.zeros((n1,n2))
-        pi[row_ind, col_ind] = 1/max(n1,n2)
-        if n1<n2: pi[:, [(j not in col_ind) for j in range(n2)]] = 1/(n1*n2)
-        elif n2<n1: pi[[(i not in row_ind) for i in range(n1)], :] = 1/(n1*n2)
-    return pi
 
 ## Covert a sparse matrix into a dense matrix
 to_dense_array = lambda X: np.array(X.todense()) if isinstance(X,scipy.sparse.csr.spmatrix) else X
