4a4d291d1a27fe3acd5a88659f104c41d2093958
anushka255 <66816733+anushka255@users.noreply.github.com>
2024-08-27 14:36:36 -0400

    removing subprocess from test cases (#23)

--- tests/test_paste_cmd_line.py
+++ tests/test_paste_cmd_line.py
@@ -1,35 +1,61 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 from pathlib import Path
-import subprocess
+from collections import namedtuple
+from paste.paste_cmd_line import main as paste_cmd_line
 
 test_dir = Path(__file__).parent
 input_dir = test_dir / "data/input"
 output_dir = test_dir / "data/output"
 
+args = namedtuple(
+    "args",
+    [
+        "filename",
+        "mode",
+        "direc",
+        "alpha",
+        "cost",
+        "n_components",
+        "lmbda",
+        "initial_slice",
+        "threshold",
+        "coordinates",
+        "weights",
+        "start",
+        "seed",
+    ],
+)
+
 
 def test_cmd_line_center(tmp_path):
     print(f"Running command in {tmp_path}")
-    result = subprocess.run(
-        [
-            "python",
-            f"{test_dir.parent}/src/paste/paste-cmd-line.py",
-            "-m",
+    result = paste_cmd_line(
+        args(
+            [
+                f"{input_dir}/slice1.csv",
+                f"{input_dir}/slice1_coor.csv",
+                f"{input_dir}/slice2.csv",
+                f"{input_dir}/slice2_coor.csv",
+                f"{input_dir}/slice3.csv",
+                f"{input_dir}/slice3_coor.csv",
+            ],
             "center",
-            "--seed",
-            "0",
-            "-f",
-            f"{input_dir}/slice1.csv",
-            f"{input_dir}/slice1_coor.csv",
-            f"{input_dir}/slice2.csv",
-            f"{input_dir}/slice2_coor.csv",
-            f"{input_dir}/slice3.csv",
-            f"{input_dir}/slice3_coor.csv",
-            "-d",
             f"{tmp_path}",
-        ]
+            0.1,
+            "kl",
+            15,
+            [],
+            1,
+            0.001,
+            False,
+            [],
+            [],
+            0,
+        )
     )
-    assert result.returncode == 0
+
+    assert result is None
     assert_frame_equal(
         pd.read_csv(tmp_path / "paste_output/W_center"),
         pd.read_csv(output_dir / "W_center"),
@@ -55,24 +81,32 @@ def test_cmd_line_center(tmp_path):
 
 def test_cmd_line_pairwise(tmp_path):
     print(f"Running command in {tmp_path}")
-    result = subprocess.run(
-        [
-            "python",
-            f"{test_dir.parent}/src/paste/paste-cmd-line.py",
-            "-m",
+    result = paste_cmd_line(
+        args(
+            [
+                f"{input_dir}/slice1.csv",
+                f"{input_dir}/slice1_coor.csv",
+                f"{input_dir}/slice2.csv",
+                f"{input_dir}/slice2_coor.csv",
+                f"{input_dir}/slice3.csv",
+                f"{input_dir}/slice3_coor.csv",
+            ],
             "pairwise",
-            "-f",
-            f"{input_dir}/slice1.csv",
-            f"{input_dir}/slice1_coor.csv",
-            f"{input_dir}/slice2.csv",
-            f"{input_dir}/slice2_coor.csv",
-            f"{input_dir}/slice3.csv",
-            f"{input_dir}/slice3_coor.csv",
-            "-d",
             f"{tmp_path}",
-        ]
+            0.1,
+            "kl",
+            15,
+            [],
+            1,
+            0.001,
+            False,
+            [],
+            [],
+            0,
+        )
     )
-    assert result.returncode == 0
+
+    assert result is None
     assert_frame_equal(
         pd.read_csv(tmp_path / f"paste_output/slice1_slice2_pairwise.csv"),
         pd.read_csv(output_dir / "slices_1_2_pairwise.csv"),
